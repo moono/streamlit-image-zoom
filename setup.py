a6ef401b30040b709ff8d5703917c4380de45836
Mookyung Song <moono.song@ncsoft.com>
2021-06-02 13:46:25 +0900

    ready for initial distribution

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name="streamlit-custom-component",
+    name="streamlit-image-zoom",
     version="0.0.1",
     author="",
     author_email="",
--- streamlit_image_zoom/__init__.py
+++ streamlit_image_zoom/__init__.py
@@ -1,36 +1,22 @@
 import os
-import numpy as np
+import base64
 import streamlit.components.v1 as components
 
-from dataclasses import dataclass
+from io import BytesIO
 from PIL import Image
+# from dataclasses import dataclass
 
 
 _RELEASE = False
 
 if not _RELEASE:
-    _component_func = components.declare_component(
+    _st_image_zoom = components.declare_component(
         'st_image_zoom', url='http://localhost:3001',
     )
 else:
     parent_dir = os.path.dirname(os.path.abspath(__file__))
     build_dir = os.path.join(parent_dir, 'frontend/build')
-    _component_func = components.declare_component('st_image_zoom', path=build_dir)
-
-
-@dataclass
-class ImageResult:
-    """Dataclass to store output of React Component
-    Attributes
-    ----------
-    image_data: np.array
-        RGBA Matrix of Image Data.
-    # json_data: dict
-    #     JSON string of canvas and objects.
-    """
-
-    image_data: np.array = None
-    # json_data: dict = None
+    _st_image_zoom = components.declare_component('st_image_zoom', path=build_dir)
 
 
 def st_image_zoom(image, key=None):
@@ -38,9 +24,7 @@ def st_image_zoom(image, key=None):
 
     Parameters
     ----------
-    # name: str
-    #     The name of the thing we're saying hello to. The component will display
-    #     the text "Hello, {name}!"
+    image: PIL.Image
     key: str or None
         An optional key that uniquely identifies this component. If this is
         None, and the component's arguments are changed, the component will
@@ -48,25 +32,15 @@ def st_image_zoom(image, key=None):
 
     Returns
     -------
-    # int
-    #     The number of times the component's "Click Me" button has been clicked.
-    #     (This is the value passed to `Streamlit.setComponentValue` on the
-    #     frontend.)
-
+    None
     """
     
-    component_value = _component_func(
-        name=name, 
-        key=key, 
-        default=None,
-    )
+    # wrap as base64 encoded data
+    buff = BytesIO()
+    image.save(buff, format='PNG')
+    image_str = base64.b64encode(buff.getvalue()).decode('utf-8')
     
-    if component_value is None:
-        return ImageResult
-
-    # We could modify the value returned from the component if we wanted.
-    # There's no need to do this in our simple example - but it's an option.
-    return component_value
+    _st_image_zoom(imageData=image_str, key=key, default=None)
 
 
 # Add some test code to play with the component while it's in development.
@@ -74,12 +48,18 @@ def st_image_zoom(image, key=None):
 # app: `$ streamlit run my_component/__init__.py`
 if not _RELEASE:
     import streamlit as st
-
+    
     st.subheader('Test image')
     
-    image_file = './test-images/Dark-Mode-macOS-catalina-quality.jpg'
-    sample_image = Image.open(image_file)
-    # Create an instance of our component with a constant `name` arg, and
-    # print its output value.
-    num_clicks = st_image_zoom('World')
-    # st.markdown('You've clicked %s times!' % int(num_clicks))
+    image_file  = st.sidebar.file_uploader(
+        label='Input file',
+        type=['png', 'jpg'],
+        accept_multiple_files=False,
+    )
+    
+    if image_file is not None:
+        image = Image.open(image_file)
+        image = image.convert('RGB')
+        
+        st.image(image, use_column_width=False)
+        st_image_zoom(image)
