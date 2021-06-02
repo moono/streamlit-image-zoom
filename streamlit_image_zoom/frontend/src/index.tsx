c6f38bee7b73c3d34d63605ae2185efd7603abf0
Mookyung Song <moono.song@ncsoft.com>
2021-06-02 11:20:35 +0900

    rename frontend file

--- streamlit_image_zoom/frontend/src/index.tsx
+++ streamlit_image_zoom/frontend/src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import MyComponent from "./MyComponent"
+import ImageZoom from "./ImageZoom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <MyComponent />
+    <ImageZoom />
   </React.StrictMode>,
   document.getElementById("root")
 )
